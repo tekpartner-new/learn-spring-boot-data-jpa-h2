e8f069b8fc5f697aca8f96eac6ea5e0d31b875a3
Chandrashekar R. Gaajula <chandra.gaajula@TEKPartner.net>
2017-12-19 12:10:52 -0800

    Updates

--- src/main/java/net/tekpartner/learn/springbootsampledatajpa/service/CityServiceImpl.java
+++ src/main/java/net/tekpartner/learn/springbootsampledatajpa/service/CityServiceImpl.java
@@ -1,24 +1,6 @@
-/*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.tekpartner.learn.springbootsampledatajpa.service;
 
 import net.tekpartner.learn.springbootsampledatajpa.domain.City;
-import net.tekpartner.learn.springbootsampledatajpa.domain.HotelSummary;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -30,49 +12,39 @@ import org.springframework.util.StringUtils;
 @Transactional
 class CityServiceImpl implements CityService {
 
-	private final CityRepository cityRepository;
+    private final CityRepository cityRepository;
 
-	private final HotelRepository hotelRepository;
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
 
-	public CityServiceImpl(CityRepository cityRepository,
-			HotelRepository hotelRepository) {
-		this.cityRepository = cityRepository;
-		this.hotelRepository = hotelRepository;
-	}
+    @Override
+    public Page<City> findCities(CitySearchCriteria criteria, Pageable pageable) {
 
-	@Override
-	public Page<City> findCities(CitySearchCriteria criteria, Pageable pageable) {
+        Assert.notNull(criteria, "Criteria must not be null");
+        String name = criteria.getName();
 
-		Assert.notNull(criteria, "Criteria must not be null");
-		String name = criteria.getName();
+        if (!StringUtils.hasLength(name)) {
+            return this.cityRepository.findAll(null);
+        }
 
-		if (!StringUtils.hasLength(name)) {
-			return this.cityRepository.findAll(null);
-		}
+        String country = "";
+        int splitPos = name.lastIndexOf(",");
 
-		String country = "";
-		int splitPos = name.lastIndexOf(",");
+        if (splitPos >= 0) {
+            country = name.substring(splitPos + 1);
+            name = name.substring(0, splitPos);
+        }
 
-		if (splitPos >= 0) {
-			country = name.substring(splitPos + 1);
-			name = name.substring(0, splitPos);
-		}
+        return this.cityRepository
+                .findByNameContainingAndCountryContainingAllIgnoringCase(name.trim(),
+                        country.trim(), pageable);
+    }
 
-		return this.cityRepository
-				.findByNameContainingAndCountryContainingAllIgnoringCase(name.trim(),
-						country.trim(), pageable);
-	}
-
-	@Override
-	public City getCity(String name, String country) {
-		Assert.notNull(name, "Name must not be null");
-		Assert.notNull(country, "Country must not be null");
-		return this.cityRepository.findByNameAndCountryAllIgnoringCase(name, country);
-	}
-
-	@Override
-	public Page<HotelSummary> getHotels(City city, Pageable pageable) {
-		Assert.notNull(city, "City must not be null");
-		return this.hotelRepository.findByCity(city, pageable);
-	}
+    @Override
+    public City getCity(String name, String country) {
+        Assert.notNull(name, "Name must not be null");
+        Assert.notNull(country, "Country must not be null");
+        return this.cityRepository.findByNameAndCountryAllIgnoringCase(name, country);
+    }
 }
